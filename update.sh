3138a3bb37f7c031daf37da0b3ed8606f0d15a52
jorghyq <jorghyq@gmail.com>
2016-01-02 22:26:01 +0100

    update script for centos

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #cp scripts/convert_sxm2png_text.py pygwy/convert_sxm2png_text.py
 cp -r -v pygwy/. ~/.gwyddion/pygwy/
-sudo cp -r -v icons/. /usr/share/gwyddion/pixmaps
+sudo cp -r -v icons/. /usr/local/share/gwyddion/pixmaps
